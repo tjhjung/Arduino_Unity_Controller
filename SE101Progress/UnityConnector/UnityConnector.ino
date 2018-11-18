41badaebcf46351ea79cfff782980cb3b9a3d1da
elisaluan <30943907+elisaluan@users.noreply.github.com>
2018-11-18 15:56:03 -0500

    Use this to send info from arduino to unity. Println send as string

---
+++ SE101Progress/UnityConnector/UnityConnector.ino
@@ -0,0 +1,42 @@
+#include <SerialCommand.h>
+
+SerialCommand sCmd;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  sCmd.addCommand("PING", pingHandler);
+  sCmd.addCommand("ECHO", echoHandler);
+  sCmd.setDefaultHandler(errorHandler);
+}
+
+void loop() {
+  
+  // Your operations here
+
+  if (Serial.available() > 0)
+    sCmd.readSerial();
+
+  delay(50);  
+}
+
+void pingHandler ()
+{
+  Serial.println("PONG");
+}
+
+void echoHandler ()
+{
+  char *arg;
+  arg = sCmd.next();
+  if (arg != NULL)
+    Serial.println(arg);
+  else
+    Serial.println("nothing to echo");
+}
+
+arg errorHandler (const char *command)
+{
+  // Error handling
+}
